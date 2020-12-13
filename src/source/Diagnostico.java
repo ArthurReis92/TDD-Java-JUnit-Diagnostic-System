8910edf3e97bb3b1d76cd43c716b0a5f8cc02f1f
Arthur Reis <afelipereis@gmail.com>
2020-12-13 19:58:00 -0300

    The 'verificaGlicose'method was implemented

--- src/source/Diagnostico.java
+++ src/source/Diagnostico.java
@@ -2,6 +2,12 @@ package source;
 
 public class Diagnostico {
 
-
+	public static String verificaGlicose(ProntuarioDigital prontuario) {
+		String resultado = "Valor inválido";
+		if (prontuario.getGlicose() < 100 && prontuario.getGlicose() > 0) resultado = "Normal";
+		else if (prontuario.getGlicose() >= 100 && prontuario.getGlicose() <= 125) resultado = "Pre Diabetes";
+		else if (prontuario.getGlicose() > 125) resultado = "Diabetes";
+		return resultado;
+	}
 
 }
--- test/testes/TestDiagnostico.java
+++ test/testes/TestDiagnostico.java
@@ -1,5 +1,43 @@
 package testes;
 
-public class TestDiagnostico {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import source.Diagnostico;
+import source.ProntuarioDigital;
+
+public class TestDiagnostico {
+	ProntuarioDigital pd;
+
+	double temperatura = 37;
+	int glicose = 88;
+	private int batimentos = 85;
+	private int saturacao = 98;
+	private int pSistolica = 119;
+	private int pDiastolica = 70;
+
+	@Before
+	public void initProntuario() {
+		pd = new ProntuarioDigital(temperatura, glicose, batimentos, saturacao, pSistolica, pDiastolica);
+	}
+
+	@Test
+	public void testVerificaGlicoseNormal() {
+		pd.setGlicose(92);
+		assertEquals("Normal", Diagnostico.verificaGlicose(pd));
+	}
+
+	@Test
+	public void testVerificaGlicosePreDiabetes() {
+		pd.setGlicose(120);
+		assertEquals("Pre Diabetes", Diagnostico.verificaGlicose(pd));
+	}
+
+	@Test
+	public void testVerificaGlicoseDiabetes() {
+		pd.setGlicose(130);
+		assertEquals("Diabetes", Diagnostico.verificaGlicose(pd));
+	}
 }
